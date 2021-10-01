e302ed8457affe091861ebd10ca2f2e906be94fd
rajjubajra <rajjubajra@gmail.com>
2021-10-01 12:59:57 +0100

    comjs-tw2-01 products

--- commercejs/comjs-tw2-01/src/Components/CheckoutForm.js
+++ commercejs/comjs-tw2-01/src/Components/CheckoutForm.js
@@ -2,77 +2,77 @@ import React from 'react'
 
 function CheckoutForm({firstName, lastName, email, shippingName, shippingStreet, shippingCity, shippingPostCode, cardNum, expMonth, expYear, ccv}) {
   return (
-    <form className="grid grid-cols-2 grid-flow-row font-extralight text-sm">
+    <form className="md:w-1/2 w-full grid grid-cols-2 grid-flow-row font-extralight text-sm">
 
-      <div className="col-span-2 border border-b border-gray-400">
-        <h4 className="col-span-2">Customer information</h4>
+      <div className="col-span-2 border border-gray-400">
+        <h4>Customer information</h4>
       </div>
       
-      <div className="flex flex-col">
+      <div className="flex flex-col grid-col-2">
         <label htmlFor="firstName">First name</label>
         <input className="outline-none  border border-gray-200" type="text" value={firstName} name="firstName" placeholder="Enter your first name" required />
       </div>
       
-      <div className="flex flex-col">
+      <div className="flex flex-col col-span-2">
         <label htmlFor="lastName">Last name</label>
         <input className="outline-none  border border-gray-200" type="text" value={lastName}name="lastName" placeholder="Enter your last name" required />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="email">Email</label>
+      <div className="flex flex-col col-span-2">
+        <label htmlFor="email">Email</label>
         <input className="checkout__input" type="text" value={email} name="email" placeholder="Enter your email" required />
       </div>
       
-      <div className="col-span-2 border border-b border-gray-400">
-        <h4 className="checkout__subheading">Shipping details</h4>
+      <div className="col-span-2 border-t border-b border-gray-400">
+        <h4>Shipping details</h4>
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="shippingName">Full name</label>
+      <div className="flex flex-col col-span-2">
+        <label htmlFor="shippingName">Full name</label>
         <input className="checkout__input" type="text" value={shippingName} name="shippingName" placeholder="Enter your shipping full name" required />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="shippingStreet">Street address</label>
+      <div className="flex flex-col col-span-2">
+        <label htmlFor="shippingStreet">Street address</label>
         <input className="checkout__input" type="text" value={shippingStreet} name="shippingStreet" placeholder="Enter your street address" required />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="shippingCity">City</label>
+      <div className="flex flex-col col-span-2">
+        <label  htmlFor="shippingCity">City</label>
         <input className="checkout__input" type="text" value={shippingCity} name="shippingCity" placeholder="Enter your city" required />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="shippingPostalZipCode">Postal/Zip code</label>
+      <div className="flex flex-col col-span-2">
+        <label htmlFor="shippingPostalZipCode">Postal/Zip code</label>
         <input className="checkout__input" type="text" value={shippingPostCode} name="shippingPostalZipCode" placeholder="Enter your postal/zip code" required />
       </div>
       
-      <div className="col-span-2 border border-b border-gray-400">
+      <div className="col-span-2 border-t border-b border-gray-400">
         <h4 className="checkout__subheading">Payment information</h4>
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="cardNum">Credit card number</label>
+      <div className="flex flex-col col-span-2">
+        <label  htmlFor="cardNum">Credit card number</label>
         <input className="checkout__input" type="text" name="cardNum" value={cardNum} placeholder="Enter your card number" />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="expMonth">Expiry month</label>
+      <div className="flex flex-col col-span-2">
+        <label  htmlFor="expMonth">Expiry month</label>
         <input className="checkout__input" type="text" name="expMonth" value={expMonth} placeholder="Card expiry month" />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="expYear">Expiry year</label>
+      <div className="flex flex-col col-span-2">
+        <label  htmlFor="expYear">Expiry year</label>
         <input className="checkout__input" type="text" name="expYear" value={expYear} placeholder="Card expiry year" />
       </div>
       
-      <div>
-        <label className="checkout__label" htmlFor="ccv">CCV</label>
+      <div className="flex flex-col col-span-2">
+        <label  htmlFor="ccv">CCV</label>
         <input className="checkout__input" type="text" name="ccv" value={ccv} placeholder="CCV (3 digits)" />
       </div>
 
-      <div className="col-span-2 border border-b border-gray-400">
-        <button className="checkout__btn-confirm">Confirm order</button>
+      <div className="col-span-2">
+        <button className="py-2  px-3 my-2 border border-gray-400">Confirm order</button>
       </div>
 
     </form>
