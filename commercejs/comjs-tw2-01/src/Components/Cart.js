a447c3fd5e4d46725fc85ae7dfa891d3a3ee9fdb
rajjubajra <rajjubajra@gmail.com>
2021-10-01 04:25:39 +0100

    comjs-tw2-01 products

--- commercejs/comjs-tw2-01/src/Components/Cart.js
+++ commercejs/comjs-tw2-01/src/Components/Cart.js
@@ -5,15 +5,15 @@ function Cart({cartItems}) {
 
   const [totalItem, setTotalItem] = useState(0);
 
-  useEffect(()=>{
-    setTotalItem(cartItems.total_items);
+  // useEffect(()=>{
+  //   setTotalItem(cartItems.total_items);
       
-  },[cartItems.total_items])
+  // },[cartItems.total_items])
 
 
   console.log("CART-items",cartItems);
-  console.log("CART-items",cartItems.cart);
-  console.log("Total item", totalItem);
+  console.log("CART-items", cartItems.cart);
+  console.log("Total item", cartItems.cart.total_items);
 
   
   return (
