776d92579e0cd5cf887b5792b64c58661be717ae
rajjubajra <rajjubajra@gmail.com>
2021-10-01 04:56:37 +0100

    comjs-tw2-01 products

--- commercejs/comjs-tw2-01/src/Components/Cart.js
+++ commercejs/comjs-tw2-01/src/Components/Cart.js
@@ -12,14 +12,28 @@ function Cart({cartItems}) {
 
 
   console.log("CART-items",cartItems);
-  console.log("CART-items", cartItems.cart);
   console.log("Total item", cartItems.total_items);
+  console.log("TotalItem", totalItem);
+
+  const CartList = (cartItems) => {
+      cartItems.map((item)=>{
+        return <div>
+          <div>{item.product_name} | Qty: {item.quantity} | Rate: {item.price.formatted_with_symbol}</div>
+        </div>
+      })
+
+  }
 
   
   return (
     <div>
       {totalItem > 0 
-      ? <div>Total Items:{totalItem}</div>
+      ? <div>
+        {CartList(cartItems)}
+        <div>Total Items:{totalItem}</div>
+        <div>Sub Total: {cartItems.subtotal.formatted_with_symbol}</div>
+
+        </div>
       : <div>Cart is empty</div>
       }
     </div>
