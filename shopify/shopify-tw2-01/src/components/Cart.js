407d308e6b1cf058bfd4b72fffae0b10cb7ebea6
rajjubajra <rajjubajra@gmail.com>
2021-10-10 17:18:22 +0545

    shopify-tw2-01 item.js

--- shopify/shopify-tw2-01/src/components/Cart.js
+++ shopify/shopify-tw2-01/src/components/Cart.js
@@ -7,29 +7,29 @@ function Cart() {
   const { isCartOpen, closeCart, checkout } = useContext(ShopContext)
 
   console.log(checkout);
-  console.log(checkout.lineItems);
+  console.log(checkout);
   
 
-  if(checkout.lineItems){
+  //if(checkout.lineItems){
     return (
       <div>
-        {checkout.lineItems.map((item)=>{
+        {/* {checkout.lineItems.map((item)=>{
           return <div>
             <h3>{item.title}</h3>
             <div>
-              {/* <img 
+              <img 
                 className="w-24" 
                 src={item.variant.image.src}  
-                alt={item.variant.image.altText} /> */}
+                alt={item.variant.image.altText} />
             </div>
-            {/* <p>{item.variant.ttitle}</p>
+            <p>{item.variant.ttitle}</p>
             <p>{item.quantity}</p>
-            <p>{item.variant.price}</p> */}
+            <p>{item.variant.price}</p>
           </div>
-        })}
+        })} */}
       </div>
     )
-  }
+  //}
 }
 
 export default Cart
