15dc7aa4a9ba143d00241b860724fc92439d2937
rajjubajra <rajjubajra@gmail.com>
2021-10-12 16:39:56 +0545

    shopify tw2-01

--- shopify/shopify-tw2-01/src/components/Cart.js
+++ shopify/shopify-tw2-01/src/components/Cart.js
@@ -28,17 +28,23 @@ useEffect(()=>{
         <h2 className="text-xl">Cart</h2>
         {lineItems !== undefined && lineItems.length > 0 
         ? lineItems.map((item)=> {
+
+          const {item:{variant:{price}, quantity, title}} = item;
+
         return <div className="flex flex-col">
-            <h3>{item.title}</h3>
+            <h3>{title}</h3>
             <div>
               <img 
                 className="w-40" 
                 src={item.variant.image.src}  
                 alt={item.variant.image.altText} />
             </div>
-            <p>{item.variant.ttitle}</p>
-            <p>{item.quantity}</p>
-            <p>{item.variant.price}</p>
+            <div className="flex flex-row flex-wrap w-full">
+              <p>Description: {item.variant.title}</p>
+              <p>Quantity: {quantity}</p>
+              <p>Rate: {price}</p>
+              <p>Amount: {price * quantity}</p>
+            </div>
           </div>
         })
         : <div className="text-xl p-2">Cart is empty</div> 
