ecacdc210a047406c19cc4d229e275df01bd28ca
rajjubajra <rajjubajra@gmail.com>
2021-10-12 23:54:28 +0545

    shopify tw2-01

--- shopify/shopify-tw2-01/src/components/Cart.js
+++ shopify/shopify-tw2-01/src/components/Cart.js
@@ -33,7 +33,8 @@ useEffect(()=>{
           const {variant:{price: rate}, quantity: qty, title} = item;
 
           const amt = rate * qty;
-          setTotalAmount(amt + totalAmount)
+          const tot = 0
+          setTotalAmount(amt + tot)
 
         return <div className="flex flex-col">
             <h3 className="text-xl py-1 mb-1">{title}</h3>
