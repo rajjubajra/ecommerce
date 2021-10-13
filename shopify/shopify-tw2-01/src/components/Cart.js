9ecaac0a431a287f015711668fb400c24e2652c9
rajjubajra <rajjubajra@gmail.com>
2021-10-13 07:21:06 +0545

    shopify tw2-01

--- shopify/shopify-tw2-01/src/components/Cart.js
+++ shopify/shopify-tw2-01/src/components/Cart.js
@@ -38,7 +38,7 @@ console.log("checkoutData", checkout !== undefined && currency);
     && lineItems.length < 1 
     && <Loading />
     return (
-      <div className="w-full flex flex-col p-2 my-3">
+      <div className="w-full flex flex-col p-4 my-3 border-2 border-gray-300">
         <h2 className="text-xl">Cart</h2>
         {lineItems !== undefined && lineItems.length > 0 
         ? lineItems.map((item)=> {
@@ -47,21 +47,22 @@ console.log("checkoutData", checkout !== undefined && currency);
           const amt = rate * qty;
 
           
-        return <div className="flex flex-col">
+        return <div className="flex flex-row flex-wrap">
             <h3 className="text-xl py-1 mb-1">{title}</h3>
             <div className="flex flex-row flex-wrap">
-                <div>
+              <div>
                 <img
                   className="w-56 mt-1 p-2" 
                   src={item.variant.image.src}  
                   alt={item.variant.image.altText} />
-                </div>
+                
                 <div className="grid grid-flow-row gap-2 w-full">
                   <p>Description: {item.variant.title}</p>
                   <p>Quantity: {qty}</p>
                   <p>Rate: {currency} {rate}</p>
                   <p>Amount: {currency} {amt.toFixed(2)}</p>
                 </div>
+              </div>
             </div>
           </div>
         })
