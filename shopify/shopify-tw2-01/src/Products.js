e121fd04c3d69340fc44713cd6abc34f3c6bdafc
rajjubajra <rajjubajra@gmail.com>
2021-10-18 02:39:02 +0545

    shopify tw2-01 product

--- shopify/shopify-tw2-01/src/Products.js
+++ shopify/shopify-tw2-01/src/Products.js
@@ -1,5 +1,5 @@
-import {useState, useContext} from 'react';
-import ShopProvider, {ShopContext} from './context/shopContext';
+import {useState} from 'react';
+import ShopProvider from './context/shopContext';
 import Items from './components/Items';
 import Item from './components/Item';
 import Cart from './components/Cart';
@@ -7,11 +7,7 @@ import Cart from './components/Cart';
 function Products() {
 
   const [productId, setProductId] = useState('');
-  const {isCartOpen, closeCart} = useContext(ShopContext);
-
-  //console.log(productId);
-  console.log("cart open",isCartOpen);
-  console.log("cart close",closeCart)
+  
 
   return (
     <div>
