fd5c5d9ed8287e3ec28e2114bd325bc9f2ae45dd
rajjubajra <rajjubajra@gmail.com>
2021-10-18 12:11:21 +0545

    updates

--- shopify/shopify-tw2-01/src/Products.js
+++ shopify/shopify-tw2-01/src/Products.js
@@ -2,23 +2,23 @@ import {useEffect, useState} from 'react';
 import Items from './components/Items';
 import Item from './components/Item';
 import Cart from './components/Cart';
-//import {actionFetchAllProduct} from './redux/action';
-//import {useDispatch, useSelector} from 'react-redux';
+import {actionFetchAllProduct} from './redux/action';
+import {useDispatch, useSelector} from 'react-redux';
 
 function Products() {
 
-  //const dispatch = useDispatch();
-  //const product = useSelector(state => state.reducerFetchAllProducts.product_data);
+  const dispatch = useDispatch();
+  const product = useSelector(state => state.reducerFetchAllProducts.product_data);
 
-  //console.log(product);
+  console.log(product);
 
   const [productId, setProductId] = useState('');
 
 
 
-  // useEffect(()=>{
-  //   dispatch(actionFetchAllProduct());
-  // },[dispatch])
+  useEffect(()=>{
+    dispatch(actionFetchAllProduct());
+  },[dispatch])
 
   return (
     <div>
--- shopify/shopify-tw2-01/src/index.js
+++ shopify/shopify-tw2-01/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 
 
-ReactDom.render(<App/>, document.getElementById("root"));
+ReactDom.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+, document.getElementById("root"));
