f454373585581e5c02f2e0c08fe07469cf73cf80
rajjubajra <rajjubajra@gmail.com>
2021-10-18 12:19:14 +0545

    updates

--- shopify/shopify-tw2-01/src/Products.js
+++ shopify/shopify-tw2-01/src/Products.js
@@ -8,7 +8,8 @@ import {useDispatch, useSelector} from 'react-redux';
 function Products() {
 
   const dispatch = useDispatch();
-  const product = useSelector(state => state.reducerFetchAllProducts.product_data);
+
+  const product = useSelector(state => state.reducerFetchAllProducts);
 
   console.log(product);
 
