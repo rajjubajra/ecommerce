6687f2c1ea17cab1c50856f9bbc01187bed093b5
rajjubajra <rajjubajra@gmail.com>
2021-10-18 12:52:14 +0545

    shopify tw2-01 redux setup

--- shopify/shopify-tw2-01/src/Products.js
+++ shopify/shopify-tw2-01/src/Products.js
@@ -9,9 +9,9 @@ function Products() {
 
   const dispatch = useDispatch();
 
-  //const product = useSelector(state => state.reducerFetchAllProducts);
+  const product = useSelector(state => state.reducerFetchAllProducts);
 
-  //console.log(product);
+  console.log(product);
 
   const [productId, setProductId] = useState('');
 
