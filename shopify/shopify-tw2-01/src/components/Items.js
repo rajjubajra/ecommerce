abd5848cb809f84d1d8edca232e0b71077cc091d
rajjubajra <rajjubajra@gmail.com>
2021-10-21 09:34:00 +0545

    shopify tw2-01 cart Items

--- shopify/shopify-tw2-01/src/components/Items.js
+++ shopify/shopify-tw2-01/src/components/Items.js
@@ -13,42 +13,40 @@ function Items() {
 
   console.log("productts ",products);
   
-  const LoadImage = (image) => {
-    return image.map((item)=>{
-      return <div>
-        <img className="w-48 p-1" src={item.src} alt="product item" />
+  const Image = (image) => (
+      <div>
+        <img className="w-48 p-1" src={image[0].src} alt="product item" />
       </div>
-    })
-  }
+  )
 
-  const LoadVariants = (data) => {
-    return data.map(item => {
-      const {available, sku, title, price } = item;
-      return available && <div className="flex p-2 bg-gray-100 m-1">
-        <p>{title}</p>
-        <p> Price: {price}</p>
-        <p> SKU: {sku}</p>
-      </div>
-    })
-  }
+  const Price = (variants) => ( <div>{variants[0].price}</div> );
+
+  // const LoadVariants = (data) => {
+  //   return data.map(item => {
+  //     const {available, sku, title, price } = item;
+  //     return available && <div className="flex p-2 bg-gray-100 m-1">
+  //       <p>{title}</p>
+  //       <p> Price: {price}</p>
+  //       <p> SKU: {sku}</p>
+  //     </div>
+  //   })
+  // }
 
   if(!product_fetched) return <Loading />
   return (
     <div>
       {
         products.map((item)=>{
-          const {id,title, descriptionHtml, images, variants} = item;
+          const {id,title, images, variants} = item;
           return <div 
           onClick={() => dispatch(actionFetchSingleProduct(id))}
           className="border border-gray-300 p-3 m-3">
             <h3>{title}</h3>
-            <div>{descriptionHtml}</div>
-            <div>{LoadImage(images)}</div>
-            <div>{LoadVariants(variants)}</div>
+            <div>{Image(images)}</div>
+            <div>{Price(variants)}</div>
           </div>
         })
-      }
-      
+      }  
     </div>
   )
 }
