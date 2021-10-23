b5ed1a9d7992aac7c2a6ed9b80e36a3d3bceca3e
rajjubajra <rajjubajra@gmail.com>
2021-10-24 04:29:26 +0545

    shopify tw2-01 checkout

--- shopify/shopify-tw2-01/src/App.js
+++ shopify/shopify-tw2-01/src/App.js
@@ -1,12 +1,21 @@
+import {useEffect} from 'react';
 import {BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Products from './Products';
 import Product from './Product';
+import {useDispatch, useSelector} from 'react-redux';
+import {actionFetchCheckout} from './redux/action';
 
 
 function App() {
 
+  const dispatch = useDispatch();
+
+  const chekcoutId = useSelector( state => state.reducerCreateCheckout.checkout_id );
+
+  useEffect(()=>{
+      dispatch(actionFetchCheckout(chekcoutId));
+  },[dispatch, chekcoutId])
   
- 
   return (
     <div>
       <BrowserRouter>
