0ec3390e1fe01151596e3d22a5c9f52181f9cb78
rajjubajra <rajjubajra@gmail.com>
2021-10-26 08:50:15 +0545

    shopify tw2-01 cart

--- shopify/shopify-tw2-01/src/components/Cart.js
+++ shopify/shopify-tw2-01/src/components/Cart.js
@@ -4,17 +4,15 @@ import {TiDeleteOutline} from 'react-icons/ti';
 import {GrFormAdd} from 'react-icons/gr';
 import {BiMinus} from 'react-icons/bi';
 import {useSelector, useDispatch} from 'react-redux';
-import {actionUpdateCart} from '../redux/action';
 
 
 
 function Cart() {
 
-const dispatch = useDispatch();
-
 const checkout = useSelector(state => state.reducerFetchCheckout.checkout_data);
 
-console.log(checkout);
+console.log("CART",checkout);
+
 
     return (
       <div className="w-full flex flex-col p-4 my-3 border-2 border-gray-300">
---
+++ shopify/shopify-tw2-01/src/components/Cart/CartItem.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {TiDeleteOutline} from 'react-icons/ti';
+import {GrFormAdd} from 'react-icons/gr';
+import {BiMinus} from 'react-icons/bi';
+import {actionUpdateCart} from '../redux/action';
+import {useSelector, useDispatch} from 'react-redux';
+
+function CartItem(image, alt, checkoutId, itemId, title, description, qty, pid, currency, rate, amount) {
+
+  const dispatch = useDispatch();
+
+  const removeItemFromCheckout = () => ("test");
+
+  return (
+    <div className="grid grid-cols-12 my-2">
+             {/** IMAGE DIV */}
+            <div className="col-span-3">
+                <img  className="w-56 mt-1" 
+                      src={image}  
+                      alt={alt} />
+            </div>
+
+            {/** DESCRIPTION DIV */}
+            <div className="col-span-9">
+              <div>
+              <div className={`cursor-pointer relative w-full text-right`}>
+                  <div className="w-4" 
+                  title="Remove"
+                  onClick={ () => removeItemFromCheckout(checkoutId, itemId)}>
+                    <TiDeleteOutline />
+                  </div>
+                </div>
+
+                <h3 className="text-xl mb-1">{title}</h3>
+                
+                
+                <div className="grid grid-flow-row w-full text-sm">
+                  <div>Description: {description}</div>
+                  <div className="grid grid-cols-2">
+                    <div>Quantity: {qty} </div>
+                    <div className="flex flex-row">
+                        <div
+                        className="cursor-pointer px-2" 
+                        onClick={() => dispatch(actionUpdateCart(checkoutId, pid, 1))} >
+                        <GrFormAdd />
+                        </div>
+                        { qty > 1 ?
+                        <div 
+                        className="cursor-pointer px-2" 
+                        onClick={() => dispatch(actionUpdateCart(checkoutId, pid, -1))} >
+                        <BiMinus />
+                        </div>
+                        : <div 
+                        className="cursor-pointer px-2" 
+                        onClick={() => dispatch(actionUpdateCart(checkoutId, pid, -1))} >
+                        <BiMinus />
+                        </div>
+                        }
+                    </div>
+                  </div>
+                    
+                  <div>Rate: {currency} {rate}</div>
+                  <div>Amount: {currency} {amount.toFixed(2)}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+  )
+}
+
+export default CartItem
