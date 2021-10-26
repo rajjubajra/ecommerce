7d5bbbf7b7387bd2e1c419f44da2c2649475e438
rajjubajra <rajjubajra@gmail.com>
2021-10-27 03:29:49 +0545

    shopify tw2-01 cart

--- shopify/shopify-tw2-01/src/components/Cart/CartQty.js
+++ shopify/shopify-tw2-01/src/components/Cart/CartQty.js
@@ -3,13 +3,13 @@ import {useSelector} from 'react-redux';
 
 function CartQty() {
 
-  const quantity = useSelector(state => state.reducerFetchCheckout);
+  const quantity = useSelector(state => state.reducerFetchCheckout.checkout_data.lineItems.length);
 
   console.log("Quantity : ",quantity);
 
   return (
     <div>
-      cart summary
+      Bag Items: {quantity}
     </div>
   )
 }
